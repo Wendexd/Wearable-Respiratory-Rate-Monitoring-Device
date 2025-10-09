6d6feeb4310fce027f4a441b5ed6fea619e97383
Wendell <wendell.22villarama@gmail.com>
2025-10-09 22:45:06 +0800

    A bunch of log files and random arduino stuff which which I still have to ignore.

---
+++ Thesis Project/.pio/build/esp32-Dev-Module/FrameworkArduino/cbuf.cpp.d
@@ -0,0 +1,3 @@
+.pio/build/esp32-Dev-Module/FrameworkArduino/cbuf.cpp.o: \
+ C:/Users/wende/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
+ C:/Users/wende/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
